307539815c08a8be761f1bbd6f0f8b91005e32e7
Evan Coakley <evancoakley1@gmail.com>
2017-09-07 15:14:55 -0500

    eod progress

--- todo2.sql
+++ todo2.sql
@@ -1,2 +1,20 @@
 
 INSERT INTO todos (title, details, priority, created_at) VALUES ('Read Book 1','Read and grasp a full understanding of On The Geneology of Morality', 10, '2017-09-07 11:30:34');
+
+INSERT INTO todos (title, details, priority, created_at, completed_at) VALUES ('Study Hagakure', 'Continue on quest of mastering Bushido lifestyle', 3, '2017-09-07 8:30:03', '2017-09-07 9:30:45');
+
+INSERT INTO todos (title, details, priority, created_at) VALUES ('Contact Arnold Schwarzenegger', 'Figure out an appropriate way to come face to face with the man and tell him that his accomplishments far outweigh his faults and that he is a true hero and a leader of men', 10, '2017-09-07 11:45:23');
+
+INSERT INTO todos (title, details, priority, created_at) VALUES ('Read Immanual Kants Masterpiece', 'Sit down with a decent combination of amphetamine and sedatives, read and understand Immanuel Kants Critique of Pure Reason', 20, '2017-08-07 22:30:14');
+
+INSERT INTO todos (title, details, priority, created_at) VALUES ('Consume a cup of Kopi luwak coffee', 'procure one hundred and fifty dollars of money I have not earned, and buy a cup of coffee made from beans digested by the Asian Palm Civet', 2, '2017-03-03');
+
+INSERT INTO todos (title, details, priority, created_at, completed_at) VALUES ('Resume', 'Attend resume class at the Iron Yard', 1, '2017-09-07 13:30:00', '2017-09-07 14:30:00');
+
+INSERT INTO todos (title, details, priority, created_at, completed_at) VALUES ('Find purpose', 'Attend a computer programming bootcamp', 3, '2017-04-25 13:45', '2017-07-17 09:00:00');
+
+INSERT INTO todos (title, details, priority, created_at, completed_at) VALUES ('Learn HTML', 'Take online tutorials and then absorb mark-up language at programming bootcamp', 3, '2017-04-25 13:46:32', '2017-07-31 17:00:00');
+
+INSERT INTO todos (title, details, priority, created_at, completed_at) VALUES ('Quit drinking', 'Become aware that your family, friends and healthy lifestyle are more essential than your need to feel numb at the end of the day', 2, '2017-09-01 16:00:00', '2017-09-04 17:00:00');
+
+INSERT INTO todos (title, details, priority, created_at) VALUES ('Oil Change', 'Take the time to figure out a good time to get an oil change and do it', 1, '2017-09-07 15:13:22');
