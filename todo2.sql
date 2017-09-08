3217a56c670e046d1c8eef0e9e4b1e529ad7c69e
Evan Coakley <evancoakley1@gmail.com>
2017-09-08 07:24:21 -0500

    Todos all inserted

--- todo2.sql
+++ todo2.sql
@@ -18,3 +18,23 @@ INSERT INTO todos (title, details, priority, created_at, completed_at) VALUES ('
 INSERT INTO todos (title, details, priority, created_at, completed_at) VALUES ('Quit drinking', 'Become aware that your family, friends and healthy lifestyle are more essential than your need to feel numb at the end of the day', 2, '2017-09-01 16:00:00', '2017-09-04 17:00:00');
 
 INSERT INTO todos (title, details, priority, created_at) VALUES ('Oil Change', 'Take the time to figure out a good time to get an oil change and do it', 1, '2017-09-07 15:13:22');
+
+INSERT INTO todos (title, details, priority, created_at) VALUES ('Dishes', 'wash all the dishes before your wife gets home because she is a saint and deserves a clean house after work', 1, '2017-09-07 18:00:34');
+
+INSERT INTO todos (title, details, priority, created_at, completed_at) VALUES ('Call Dave Nelson', 'Call your dads old friend and catch up with him', 7, '2017-09-01 06:15:33', '2017-09-06 19:34:44');
+
+INSERT INTO todos (title, details, priority, created_at, completed_at) VALUES ('Make dinner', 'Eat dinner before 6pm so you dont get fat in your sleep', 3, '2017-09-07 17:00:43', '2017-09-07 17:56:45');
+
+INSERT INTO todos (title, details, priority, created_at) VALUES ('Say hello to cat', 'Tell your little kitty that you love her even if she is a miniature murder slowly destroying the ecosystem of the neighborhood', 8, '2017-09-07 18:00:00');
+
+INSERT INTO todos (title, details, priority, created_at) VALUES ('Read Book2', 'Read Being and Nothingness whilst maintaining your sanity at the same time', 8, '2017-09-05 19:07:33');
+
+INSERT INTO todos (title, details, priority, created_at) VALUES ('Complete bootcamp', 'catch up on past projects and stay on top of Java intense portion of course', 1, '2017-09-07 19:00:45');
+
+INSERT INTO todos (title, details, priority, created_at, completed_at) VALUES ('Car wash', 'Go to the mister car wash and clean my car', 2, '2017-09-6 15:45:44', '2017-09-06 19:45:22');
+
+INSERT INTO todos (title, details, priority, created_at, completed_at) VALUES ('Mow the lawn', 'Find time to mow the lawn', 9, '2017-09-06 12:34:22', '2017-09-07 19:33:44');
+
+INSERT INTO todos (title, details, priority, created_at) VALUES ('Date Night', 'Take your beautiful wife out for a date you self absorbed sonuva', 1, '2017-09-08 07:20:04');
+
+INSERT INTO todos (title, details, priority, created_at) VALUES ('Study', 'Study for at least 13 hours over the weekend', 1, '2017-09-06 07:45:44');
